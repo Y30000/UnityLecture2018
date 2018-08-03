2f5787a201c57a9f335834d82e4564e805e457bb
SangHun Jeong <bot01001010@gmail.com>
2018-08-03 14:48:06 +0900

    Changed name of Day14

---
+++ Day14_Minecraftt/Assets/Scripts/HealToPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealToPlayer : MonoBehaviour {
+
+    public GameObject healFx;
+    private bool isHealing = false;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && !isHealing)
+        {
+            print("Heal");
+            Destroy(Instantiate(healFx, collision.gameObject.transform.Find("FXPos")),2f);
+            isHealing = true;
+            Invoke("StatusBack", 2f);
+        }
+    }
+
+    void StatusBack()
+    {
+        isHealing = false;
+    }
+}
