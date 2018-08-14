3c4b2b7c85f37b9f1f38b380d27d977dc7997811
SangHun Jeong <bot01001010@gmail.com>
2018-08-14 14:46:31 +0900

    20180814 End of class

---
+++ Day21_TPS_Action_Combat/Assets/DropWeapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropWeapon : StateMachineBehaviour {
+
+    PlayerController pc;
+    Transform weaponHolder;
+    Transform weapon;
+    Rigidbody rb;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        pc = animator.GetComponent<PlayerController>();
+        weaponHolder = pc.weapohHolder;
+        weapon = weaponHolder.GetChild(0);
+        weapon.SetParent(null);
+        animator.SetBool("HasWeapon", false);
+        rb = weapon.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
---
+++ Day21_TPS_Action_Combat/Assets/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    public float rotationSpeed = 150f; 
+
+    CharacterController con;
+    Vector3 moveDirection = Vector3.zero;
+    Animator anim;
+
+    float jumpSpeed = 15f;
+    float gravity = 23f;
+    float currentDownSpeed;
+
+    MouseLook mouseLook;
+    public Transform weapohHolder;
+
+    // Use this for initialization
+    void Start () {
+        con = GetComponent<CharacterController>();
+        mouseLook = GetComponentInChildren<MouseLook>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	public void FrameMove () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float mouseMoveX = Input.GetAxis("Mouse X");
+        float mouseMoveY = Input.GetAxis("Mouse Y");
+
+
+        if (Input.GetMouseButton(1))
+        {
+            moveDirection = new Vector3(h, 0, v).normalized;
+            float rotationY = mouseMoveX * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * rotationY);
+            mouseLook.ResetCamera();
+
+            //if(h > 0)
+            //{
+            //    anim.SetBool("isRight", true);
+            //    anim.SetBool("isLeft", false);
+            //}
+            //else if(h < 0)
+            //{
+            //    anim.SetBool("isRight", false);
+            //    anim.SetBool("isLeft", true);
+            //}
+            //else
+            //{
+            //    anim.SetBool("isRight", false);
+            //    anim.SetBool("isLeft", false);
+            //}
+        }
+        else
+        {
+            moveDirection = new Vector3(0, 0, v).normalized;
+            float rotationY = h * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * rotationY);
+            //anim.SetBool("isRight", false);
+            //anim.SetBool("isLeft", false);
+        }
+
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed * 1.5f;
+
+        //if (Input.GetButtonDown("Jump"))
+        //    anim.SetBool("isJump", true);
+
+        //if (Input.GetButtonUp("Jump"))
+        //        Jump();
+
+        currentDownSpeed -= gravity * Time.deltaTime;
+        moveDirection.y = currentDownSpeed;
+
+        con.Move(moveDirection * Time.deltaTime);
+        //anim.SetFloat("moveSpeed", con.velocity.magnitude);
+        //anim.SetBool("isGround", con.isGrounded);
+        anim.SetFloat("h", h);
+        anim.SetFloat("v", v);
+    }
+
+    internal GameObject GetNearstWeaponIn(float radius, float angle, string weaponTag)
+    {
+        GameObject[] weapons = GameObject.FindGameObjectsWithTag(weaponTag);
+        var list = new List<GameObject>(weapons);
+        int index = list.FindIndex(o =>
+        {
+            Vector3 dir = o.transform.position - transform.position;
+            return dir.magnitude < radius && Vector3.Angle(dir, transform.forward) < angle;
+        });
+        if (index == -1)
+            return null;
+        list[index].GetComponent<Rigidbody>().isKinematic = true;
+        return list[index];
+    }
+
+    void Jump()
+    {
+        if (!con.isGrounded)
+            return;
+        currentDownSpeed = jumpSpeed; 
+        anim.SetBool("isJump",false);
+    }
+}
