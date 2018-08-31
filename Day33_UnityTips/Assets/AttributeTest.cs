42fcb9e770b1058be9d2d9fa48427f000d70d2ed
SangHun Jeong <bot01001010@gmail.com>
2018-08-31 13:55:10 +0900

    Add Day33

---
+++ Day33_UnityTips/Assets/AttributeTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttributeTest : MonoBehaviour {
+    [Range(0, 10)]  //대괄호로 시작하는건 Attribute 
+    public int DamageRange = 10;
+
+    [Header("Health Settings")]
+    public int health;
+    public int maxHealth;
+
+    [Space(50)] //줄 띄우기
+    [Header("Danage Settings")]
+    public int damage;
+
+    [HideInInspector] //다른 스크립트에서 불러오지만 사람이 건드리진 못하게
+    public float hiddenValue;
+
+    [SerializeField]
+    private int privateInt = 100;
+
+    public Person person;
+
+	// Use this for initialization
+	void Start () {
+        person = new Person();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+//[Serializable]
+[SerializeField]
+public class Person {
+    public string address;
+}
---
+++ Day33_UnityTips/Assets/MobManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobManager : Singleton<MobManager>
+{
+    protected MobManager() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public string myGlobalVar = "whatever";
+    public int MobCount() { return 100; }
+}
