8aba53518f518ca5667be808229648834142dec7
Sanghun Jeong <bot01001010@gmail.net>
2018-09-04 19:01:56 +0900

    Add HW_Day35

---
+++ HW_Day35_YouDie/Assets/YouDieMgr.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YouDieMgr : MonoBehaviour {
+
+    Image img;
+    Text tex;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+        tex = GetComponentInChildren<Text>();
+    }
+
+    public void YouDie()
+    {
+        StartCoroutine(Appear());
+    }
+
+    IEnumerator Appear()
+    {
+        Color imgColor;
+        Color texColor;
+        imgColor = img.color;
+        texColor = tex.color;
+        for (float alpha = 0; alpha < 1; alpha += Time.deltaTime / 2)
+        {
+            if (alpha > 1)
+                alpha = 1;
+            imgColor.a = texColor.a = alpha;
+            img.color = imgColor;
+            tex.color = texColor;
+            yield return null;
+        }
+    }
+}
