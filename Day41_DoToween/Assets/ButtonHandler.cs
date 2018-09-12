e696eb8700ee83a10acf37ce39af77343fe6de49
SangHun Jeong <bot01001010@gmail.com>
2018-09-12 12:48:50 +0900

    Add Day41

---
+++ Day41_DoToween/Assets/ButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("OnPointerClick");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("OnPointerDown");
+        transform.DOScale(0.8f, .1f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        print("OnPointerUp");
+        transform.DOScale(1f, .1f);
+    }
+}
