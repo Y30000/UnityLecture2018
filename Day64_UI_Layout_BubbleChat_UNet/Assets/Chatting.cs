ce3949d1590151b006fa7726a335af6f18beff2f
SangHun Jeong <bot01001010@gmail.com>
2018-11-02 13:07:58 +0900

    Add Day68

---
+++ Day64_UI_Layout_BubbleChat_UNet/Assets/Chatting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Chatting : NetworkBehaviour
+{
+    ChatSystem chatSystem;
+    string nickName;
+
+    private void Start()
+    {
+        chatSystem = GameObject.FindObjectOfType<ChatSystem>();
+        var inputField = chatSystem.inputField;
+        inputField.onEndEdit.AddListener(message =>             //엔터 쳐지는걸 감지
+        {
+            if (!isLocalPlayer)
+                return;
+
+            print(message);
+            if (message != string.Empty)
+            {
+                CmdSendChat(nickName + ": " + message);
+            }
+        });
+    }
+
+    [Command]
+    void CmdSendChat(string message)
+    {
+        RpcReceivedChat(message);
+    }
+
+    [ClientRpc]
+    void RpcReceivedChat(string message)
+    {
+        chatSystem.AddMessage(message, !isLocalPlayer);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        nickName = "User " + netId;
+
+        //chatSystem = GameObject.FindObjectOfType<ChatSystem>();
+        //var inputField = chatSystem.inputField;
+        //inputField.onEndEdit.AddListener(message =>             //엔터 쳐지는걸 감지
+        //{
+        //    if (!isLocalPlayer)
+        //        return;
+
+        //    print(message);
+        //    if (message != string.Empty)
+        //    {
+        //        CmdSendChat(nickName + ": " + message);
+        //    }
+        //});
+    }
+}
---
+++ Day68_FirstPersonController_UNet/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour {
+    public Transform holder;
+
+    Camera fpsCamera;
+
+    private void Start()
+    {
+        fpsCamera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(fpsCamera.transform.position,fpsCamera.transform.forward,out hit, 4f))
+            {
+                print(hit.transform.name);
+                Pickable item = hit.transform.gameObject.GetComponent<Pickable>();
+                if (item != null)
+                    Pickup(hit.transform);
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            ThrowItem();
+        }
+	}
+
+    private void ThrowItem()
+    {
+        if(holder.childCount > 0)
+        {
+            Transform item = holder.GetChild(0);
+            item.SetParent(null);
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.GetComponent<Rigidbody>().AddForce(fpsCamera.transform.forward * 1000f);
+        }
+    }
+
+    private void Pickup(Transform item)
+    {
+        if(holder.childCount < 1)
+        {
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            item.SetParent(holder);
+            //item.transform.position = holder.transform.position;
+            item.transform.localPosition = Vector3.zero;
+        }
+    }
+}
